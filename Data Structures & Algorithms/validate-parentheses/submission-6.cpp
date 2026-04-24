3debb45dcb09cb24ff28d63c51ecb4fbbf5514a8
Braedon Klock <braedonk@hotmail.com>
2026-04-24 12:27:25 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> brackets {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        std::stack<char> stack;
+
+        for (char c : s) {
+            if (brackets.count(c)) {
+                if (!stack.empty() && stack.top() == brackets[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+};
